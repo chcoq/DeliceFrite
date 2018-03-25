9c24b2a77c6dfe164a283e8a20ed891911bcc94c
lecocq <f.fr>
2018-03-26 00:34:12 +0200

    - Mise en place du menu naviagation - Création des pages

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,37 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/menu", name="menu")
+     */
+    public function menuAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/menu.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/photo", name="photo")
+     */
+    public function photoAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/photo.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/contact.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
